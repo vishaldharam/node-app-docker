21ff46ec0888ce129d1de7e2ac0c69ab700a70d1
Vishal Dharam <vishal.dharam20@pccoepune.org>
2025-02-15 09:42:42 +0530

    route added /docker

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.get("/", (req, res) => {
   res.send("Hello, Cloud Run with Docker Buildx!");
 });
 
+app.get("/docker", (req, res) => {
+  res.send({
+    msg: "Hello! you have successfully configure the github action workflow and GCP cloud run",
+    response: "Thanks for getting it done!",
+  });
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
